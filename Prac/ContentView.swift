b7eb947711500bdab34ed31820ae0a58483a14ef
Wongi <hpurs95@gmail.com>
2022-07-12 06:19:40 +0900

    Initial Commit

---
+++ Prac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prac
+//
+//  Created by MacBook Air on 2022/07/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Prac/PracApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracApp.swift
+//  Prac
+//
+//  Created by MacBook Air on 2022/07/12.
+//
+
+import SwiftUI
+
+@main
+struct PracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
